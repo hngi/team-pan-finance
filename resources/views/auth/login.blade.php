abe68ab3343de381bd60d31104befd1e0b763ba8
emmannl <eul.nelson@gmail.com>
2019-09-25 16:00:07 +0100

    Complete authentication

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,56 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+    <div class="container">
+        <div class="col-4 mx-auto text-center">
+            <a href="" style="color: #5829B8; font-weight: 800; font-size: 250%;" class="d-block nl-4"><img src="https://res.cloudinary.com/sgnolebagabriel/image/upload/v1569248451/finance-tracker/Finance_money_Dollar_management_cash_savings_salary-512_sgdosn.png" alt="Logo" style="width:40px;" class="mx-auto img-fluid"><span class="mt-2">fintrack</span></a>
+        </div>
+        <div class="col-md-6 offset-md-3 pt-5 pb-5 mt-5 mb-3" style="text-align: center; border: thin solid #868080;">
+            <h4 style="font-weight: 200; font-size: 150%;">Welcome back!</h4>
+            <p style="font-weight: 600; font-size: 120%;">Please Login</p>
+            <form action="{{ route('login') }}" method="post">
+                @csrf
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
                 </div>
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" required>
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                    <div class="form-check float-left">
+                        <label class="form-check-label">
+                            <input type="checkbox" class="form-check-input" {{ old('remember') ? 'checked' : '' }} name="remember" >Remember Me
+                        </label>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 mt-2">
+                            <button class="btn col-md-12 pt-2 pb-2" type="submit" style="background-color: #5829B8; color: #fff; font-size: 110%; font-weight: 300;">Log
+                                In</button>
+                        </div>
+                    </div>
+                </div>
+
+            </form> <br>
+            <div class="col-md-12" style="text-align: center;">
+                <p class="mx-auto mt-2">Don't have an account? <a href="{{ route('register') }}" style="color: #5829B8">Register</a>
+                </p>
+            </div>
+            <div class="col-md-10 offset-md-1">
+                <p class="mx-auto mt-2" style="font-weight: 300; color: #868080">By clicking on Log In, you agree to our terms & conditions and privacy policy
+                </p>
             </div>
         </div>
     </div>
-</div>
 @endsection
