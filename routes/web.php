13c7df7217060d45eb241798820ec93a4e662303
emmannl <eul.nelson@gmail.com>
2019-09-26 11:32:23 +0100

    redirect to dashboard ig logged in

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::view('/','welcome');
+Route::get('/',function () {
+    if (auth()->check()) {
+        return redirect()->route('expenses.index');
+    }
+    return view('welcome');
+});
 Route::get('expenses/report', 'ExpensesController@report')->name('expenses.report');
 Route::get('expenses/report/custom-range', 'ExpensesController@customRangeReport')->name('expenses.report.custom');
 Route::resource('expenses', 'ExpensesController');
