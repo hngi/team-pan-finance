44b1cb806c1292e93c1835c1dd39f6823212a094
emmannl <eul.nelson@gmail.com>
2019-10-02 13:16:09 +0100

    Add faqsa HTML to routes

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ Route::redirect('home', 'expenses');
 Route::get('expenses/report', 'ExpensesController@report')->name('expenses.report');
 Route::get('expenses/report/custom-range', 'ExpensesController@customRangeReport')->name('expenses.report.custom');
 Route::resource('expenses', 'ExpensesController')->except(['show']);
+
+Route::view('faqs', 'faqs');
