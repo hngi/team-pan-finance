d7a2f8103d6afc9ee55b736ee21517af0dc63ec8
Stephen Oluwafemi <phemmylintry@gmail.com>
2019-10-04 19:34:40 +0100

    edited script.js again

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,8 +5,8 @@ jQuery(document).ready(function() {
         Background slideshow
     */    
     $('.testimonials-container').backstretch([
-                                   "{{ asset('img/backgrounds/2.jpg') }}"
-                                 , "{{ asset('img/backgrounds/1.jpg') }}"
+                                   "https://res.cloudinary.com/femosocratis/image/upload/v1570211354/1_dyxmja.jpg"
+                                 , "https://res.cloudinary.com/femosocratis/image/upload/v1570211360/2_fjejyi.jpg"
                                  ], {duration: 3000, fade: 750});
     
     $('a[data-toggle="tab"]').on('shown.bs.tab', function(){
