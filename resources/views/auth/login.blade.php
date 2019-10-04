ccf15e3e0492c38d7109762f8be5fa2f198ef5b8
GMike the GD <mikeolams@yahoo.com>
2019-10-04 18:37:38 +0100

    login google url added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,7 +44,7 @@
 
             </form> <br>
             <div class="col-md-12" style="text-align: center;">
-                <p class="mx-auto mt-2">Don't have an account? <a href="{{ route('register') }}" style="color: #5829B8">Register</a> or <a href="{{ route('auth/google') }}" style="color: #5829B8">SignIn with Google</a>
+                <p class="mx-auto mt-2">Don't have an account? <a href="{{ route('register') }}" style="color: #5829B8">Register</a> or <a href="{{ url('auth/google') }}" style="color: #5829B8">SignIn with Google</a>
                 </p>
             </div>
             <div class="col-md-10 offset-md-1">
