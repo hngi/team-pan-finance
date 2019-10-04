2484c190953c12c420308cec787e1fbb8902516a
Stephen Oluwafemi <phemmylintry@gmail.com>
2019-10-04 19:25:37 +0100

    edited script.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,8 +5,8 @@ jQuery(document).ready(function() {
         Background slideshow
     */    
     $('.testimonials-container').backstretch([
-                                   "assets/img/backgrounds/2.jpg"
-                                 , "assets/img/backgrounds/1.jpg"
+                                   "{{ asset('img/backgrounds/2.jpg') }}"
+                                 , "{{ asset('img/backgrounds/1.jpg') }}"
                                  ], {duration: 3000, fade: 750});
     
     $('a[data-toggle="tab"]').on('shown.bs.tab', function(){
